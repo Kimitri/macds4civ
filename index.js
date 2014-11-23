01920a5cb8a244a02a474bc27bb306cdc51ee5d8
Kimmo Tapala <kimitri@gmail.com>
2014-11-23 17:10:21 +0200

    Implemented dimension detection and switched to relative positioning in mouse jump.

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ var debug = false,
     devices = hid.devices(),
     controller = _(devices).filter(isDS4HID).first(),
     previous = {},
-    options = require('konfu');
+    options = require('konfu'),
+    dimensions = {
+      width: 2560,
+      height: 1440
+    };
 
 if (!controller) {
   throw new Error('Could not find desired controller.');
@@ -34,6 +38,10 @@ hidDevice.on('data', function(buf) {
   var hidData = parseDS4HIDData(buf.slice(offset)),
       relative = {},
       analog = options.analog;
+  
+  if (typeof options.detectDimensions == 'string') {
+    triggerDimensionDetection(hidData);
+  }
   
   // Calculate relative analog values and take deadzones into account
   for (var i in analog) {
@@ -59,6 +67,33 @@ hidDevice.on('data', function(buf) {
   options.debug && console.log(hidData);
 });
 
+
+
+/**
+ * Trigger screen dimension detection.
+ *
+ * @param  {object} hidData
+ * HID data.
+ */
+function triggerDimensionDetection(hidData) {
+  if (hidData[options.detectDimensions] && !previous[options.detectDimensions]) {
+    var stored = machid.mouseGetCurrentPosition();
+    
+    machid.mouseMoveABS(Number.MAX_VALUE, Number.MAX_VALUE);
+    
+    var t = setTimeout(function() {
+      var pos = machid.mouseGetCurrentPosition();
+      dimensions = {
+        width: pos.x,
+        height: pos.y
+      };
+      
+      console.log("Detected screen resolution: " + (pos.x + 1) + " x " + (pos.y + 1));
+      
+      machid.mouseMoveABS(stored.x, stored.y);
+    }, 600);
+  }
+}
 
 
 
@@ -97,7 +132,11 @@ function triggerMouseJumps(hidData) {
   var jumps = options.mouse.jumps;
   for (var i in jumps) {
     if (jumps.hasOwnProperty(i) && hidData[i]) {
-      machid.mouseMoveABS(jumps[i].x, jumps[i].y);
+      var pos = {
+        x: Math.round(jumps[i].x * dimensions.width),
+        y: Math.round(jumps[i].y * dimensions.height)
+      };
+      machid.mouseMoveABS(pos.x, pos.y);
     }
   }
 }
